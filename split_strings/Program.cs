30c04cb3a474f7038800ebfc0dd83b4d2c243220
diogobnfr <diogobonofre@gmail.com>
2024-10-27 17:56:05 -0300

    feat: added string pairs split challenge w/ C#

---
+++ split_strings/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace split_strings;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = split_string("abcdef");
+
+        foreach (var str in arr)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    private static string[] split_string(string str)
+    {
+        var arr = new string[(int)Math.Floor((str.Length - 1) / 2.0 + 1)];
+
+        for (int i = 0, j = 0; j < arr.Length; i += 2, j++)
+        {
+            if (i == str.Length - 1)
+            {
+                arr[j] = $"{str[i]}_";
+                break;
+            }
+            arr[j] = $"{str[i]}{str[i + 1]}";
+        }
+        return arr;
+    }
+}
